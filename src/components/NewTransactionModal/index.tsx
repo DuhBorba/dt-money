a6c7a024a01e8f06e3773712964915b5cc0d96a8
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-09-06 15:54:51 -0300

    Styles Type Button Modal Income and Outcome

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
+import { X, ArrowCircleUp, ArrowCircleDown } from 'phosphor-react'
 
-import { CloseButton, Content, Overlay } from './styles'
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
 
 export const NewTransactionModal = () => {
   return (
@@ -20,6 +20,18 @@ export const NewTransactionModal = () => {
           <input type="text" placeholder='Descrição' required />
           <input type="number" placeholder='Preço' required />
           <input type="text" placeholder='Categoria' required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
